e8d95025eb1b1f35fbfe727ef4227960ff9b0aad
r1ddh118 <riddhipoddar@gmail.com>
2025-04-13 17:35:54 +0530

    student file added

---
+++ Student.java
@@ -0,0 +1,58 @@
+// Student.java
+// Class representing a student with attributes like name, PRN, department, batch, and CGPA.
+
+public class Student {
+    private String name;
+    private long prn;
+    private String dept;
+    private String batch;
+    private double cgpa;
+
+    // Constructor to initialize student attributes
+    public Student(String name, long prn, String dept, String batch, double cgpa) {
+        this.name = name;
+        this.prn = prn;
+        this.dept = dept;
+        this.batch = batch;
+        this.cgpa = cgpa;
+    }
+
+    // Getter and Setter methods for encapsulation
+    public String getName() { 
+        return name; 
+    }
+    public void setName(String name) { 
+        this.name = name; 
+    }
+    public long getPRN() { 
+        return prn; 
+    }
+    public void setPRN(long prn) { 
+        this.prn = prn; 
+    }
+    public String getDept() {  
+        return dept; 
+    }
+    public void setDept(String dept) { 
+        this.dept = dept; 
+    }
+    public String getBatch() { 
+        return batch; 
+    }
+    public void setBatch(String batch) { 
+        this.batch = batch; 
+    }
+
+    public double getCGPA() { 
+        return cgpa; 
+    }
+    public void setCGPA(double cgpa) { 
+        this.cgpa = cgpa; 
+    }
+
+    // Method to display student details
+    public void display() {
+        System.out.println("Name: " + name + ", PRN: " + prn + ", Department: " + dept + ", Batch: " + batch + ", CGPA: " + cgpa);
+    }
+}
+
