7a45e187d2a0859dc08053d78f8a19256bbd56a7
r1ddh118 <riddhipoddar@gmail.com>
2025-04-13 17:35:19 +0530

    database connection added

---
+++ DBConnection.java
@@ -0,0 +1,14 @@
+// DBConnection.java
+
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+
+    private static final String USER = "root"; 
+    private static final String PASS = "r1ddh118";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
