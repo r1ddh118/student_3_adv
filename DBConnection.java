e5b1087b4988d19f96650b5fa15faa0eb1180edf
Riddhi Poddar <riddhi.poddar.btech2023@sitpune.edu.in>
2025-04-13 17:46:54 +0530

    Updated the DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -6,7 +6,7 @@ public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/student_db";
 
     private static final String USER = "root"; 
-    private static final String PASS = "r1ddh118";
+    private static final String PASS = "password"; //add your own password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
